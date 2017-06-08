7d5e302f23ca6d43c9485d6c3f57386cae375089
tntvu <tntvu@tma.com.vn>
2017-06-08 15:47:38 +0700

    remove extra declaration

--- src/main/kotlin/org/gaconkzk/manga/domain/Manga.kt
+++ src/main/kotlin/org/gaconkzk/manga/domain/Manga.kt
@@ -1,17 +1,16 @@
 package org.gaconkzk.manga.domain
 
 import org.springframework.data.annotation.Id
-import org.springframework.data.annotation.PersistenceConstructor
 import org.springframework.data.mongodb.core.mapping.Document
 
 /**
  *
  */
 @Document
-data class Manga @PersistenceConstructor constructor(@Id val id: String,
-                                                     val name: String,
-                                                     val author: String,
-                                                     val status: String,
-                                                     val volumes: Int,
-                                                     val chapters: Int,
+data class Manga(@Id val id: String,
+                 val name: String,
+                 val author: String,
+                 val status: String,
+                 val volumes: Int,
+                 val chapters: Int,
                  val lang: String)
