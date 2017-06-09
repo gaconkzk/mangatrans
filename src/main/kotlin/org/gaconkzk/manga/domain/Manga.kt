a4d232f53ac65fd224c9544a1d194e23e848723b
tntvu <tntvu@tma.com.vn>
2017-06-09 13:49:14 +0700

    renderding

--- src/main/kotlin/org/gaconkzk/manga/domain/Manga.kt
+++ src/main/kotlin/org/gaconkzk/manga/domain/Manga.kt
@@ -10,8 +10,12 @@ import org.springframework.data.mongodb.core.mapping.Document
 data class Manga(@Id val id: String,
                  val name: String,
                  val author: String?,
-                 val status: String?,
-                 val volumes: Int,
+                 val status: Status?,
+                 val volumes: Int?,
                  val chapters: Int?,
                  val lang: String?,
                  val url: String?)
+
+enum class Status(val value: String) {
+    COMPLETE("complete"), ONGOING("ongoing")
+}
